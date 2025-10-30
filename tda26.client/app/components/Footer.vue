0cff4a7377dad59ba6e8f052ee498311364e83dc
Serhii Yavorskiy <yavorskiyserhiy158@gmail.com>
2025-10-30 02:18:31 +0100

    FEAT: Redesign footer layout and update styles
    Refactored the Footer.vue component to introduce a new layout with distinct sections for navigation, other links, and team members. Updated the copyright year and improved the SCSS for better structure, responsiveness, and visual appearance.

--- tda26.client/app/components/Footer.vue
+++ tda26.client/app/components/Footer.vue
@@ -4,29 +4,58 @@
 
 <template>
     <footer :class="$style.footer">
-        <div :class="$style.contentContainer">
-            
-            <div :class="$style.navigationContainer">
-                
+        <div :class="$style.divider">
+            <div :class="$style.top">
+                <div :class="$style.left">
+                    <div :class="$style.logo"></div>
+                </div>
+                <div :class="$style.right">
+                    <div :class="$style.navigation"> 
+                        <p :class="$style.upper">Navigace</p>
+                        <p>
+                            <RouterLink to="/">Domů</RouterLink>
+                        </p>
+                        <p>
+                            <RouterLink to="/leaderboard">Žebříčky</RouterLink>
+                        </p>
+                        <p>
+                            <RouterLink to="/account">Účet</RouterLink>
+                        </p>
+                        <p>
+                            <RouterLink to="/play">Hrát</RouterLink>
+                        </p>
+                    </div>
+                    
+                    <div :class="$style.other">
+                        <p :class="$style.upper">Ostatní</p>
+                        <p>
+                            <RouterLink to="/privacy/gdpr">GDPR</RouterLink>
+                        </p>
+                        <p>
+                            <RouterLink to="/privacy/cookies">Cookies</RouterLink>
+                        </p>
+                    </div>
+                    
+                    <div :class="$style.team">
+                        <p :class="$style.upper">Tým</p>
+                        <p>
+                            Fullstack:&nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="https://stanislavskudrna.cz" target="_blank"> Stanislav Škudrna</a>
+                        </p>
+                        <p>
+                            Fullstack:&nbsp;&nbsp;&nbsp;&nbsp;
+                            <a href="https://serhii.cz" target="_blank"> Serhii Yavorskyi</a>
+                        </p>
+                    </div>
+                </div>
             </div>
-            
+            <div :class="$style.bottom">
+                <p>© Think different Academy 2025-2026</p>
+                <p>Vytvořeno týmem Error Makers ze školy <a href="https://educhem.cz" target="_blank">Střední škola EDUCHEM, a.s.</a> pro <a href="https://tourde.app/" target="_blank">Tour de App</a>.</p>
+            </div>
         </div>
-        <div :class="$style.copyrightContainer">
-            <div :class="$style.copyrightTDA">
-                <span>©</span>
-                Think different Academy
-                <span>2024-2025</span>
-            </div>
-            <div :class="$style.copyrightTeam">
-                Vytvořeno týmem
-                Error Makers
-                ze školy
-                <a href="https://educhem.cz" target="_blank">Střední škola EDUCHEM, a.s.</a>
-                pro
-                <a href="https://tourde.app/" target="_blank">Tour de App</a>.
-            </div>
-
-        </div>
+        
+        
         
         
         
@@ -105,12 +134,112 @@
 </template>
 
 <style module lang="scss">
-@keyframes adsadsadsasa {
-    0% { transform: translateX(50vw); opacity: 0;  }
-    80% { transform: translateX(50vw); opacity: 0; }
-    100% { transform: translateX(0px); opacity: 1;  }
+.footer {
+    background-color: var(--background-color-secondary);
+    min-height: 20vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    .divider {
+        width: 80%;
+        
+        .top {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 48px 0;
+            
+            .left {
+                display: flex;
+
+                .logo {
+                    mask-image: url(../../public/icons/Think-different-Academy_LOGO_textove-cerne.svg);
+                    mask-size: contain;
+                    mask-repeat: no-repeat;
+                    mask-position: center;
+                    height: 128px;
+                    aspect-ratio: 2 / 1;
+                    background: linear-gradient(90deg, var(--accent-color), var(--accent-color-secondary));
+                }
+            }
+
+            .right {
+                display: flex;
+                width:  60%;
+                justify-content: space-between;
+                
+                .navigation,
+                .other,
+                .team
+                {
+                    .upper{
+                        font-size: 24px;
+                        font-weight: 700;
+                        margin-bottom: 20px;
+                        border-bottom: 1px solid;
+                        width: fit-content;
+                        border-bottom-color: var(--text-color-primary);
+                        pointer-events: none;
+                    }
+                    
+                    p {
+                        margin: 8px 0;
+
+                        a {
+                            color: var(--text-color-primary);
+                            text-decoration: none;
+                            transition-duration: 0.3s;
+
+                            &:hover {
+                                color: var(--accent-color-secondary);
+                                transition-duration: 0.3s;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        .bottom {
+            display:  flex;
+            justify-content: space-between;
+            border-top: 1px solid var(--color-gray);
+            padding: 16px 0;
+            font-size: 14px;
+            
+
+            p {
+                margin: 0;
+
+
+                a {
+                    color: var(--text-color-primary);
+                    text-decoration: none;
+                    font-size: 16px;
+                    transition-duration: 0.3s;
+
+                    &:hover{
+                        color: var(--accent-color-secondary);
+                        transition-duration: 0.3s;
+                    }
+                }
+            }
+        }
+    }
 }
 
+
+
+
+
+
+
+//@keyframes adsadsadsasa {
+//    0% { transform: translateX(50vw); opacity: 0;  }
+//    80% { transform: translateX(50vw); opacity: 0; }
+//    100% { transform: translateX(0px); opacity: 1;  }
+//}
 //.footer {
 //    background-color: var(--background-color-3);
 //    color: var(--text-color);
