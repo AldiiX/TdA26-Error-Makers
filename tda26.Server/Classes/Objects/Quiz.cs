02d5d17f7b76639bdc935faeeebe3f3a87241d1e
Serhii Yavorskiy <yavorskiyserhiy158@gmail.com>
2025-11-17 01:27:04 +0100

    FEAT:Add properties and constructor to Quiz class
    Introduced Uuid, Title, and QuestionCount properties to the Quiz class and added a JsonConstructor for proper deserialization. This enhances the Quiz object with essential metadata and supports JSON serialization.

--- tda26.Server/Classes/Objects/Quiz.cs
+++ tda26.Server/Classes/Objects/Quiz.cs
@@ -1,5 +1,25 @@
-﻿namespace tda26.Server.Classes.Objects;
+﻿using System.Text.Json.Serialization;
+
+namespace tda26.Server.Classes.Objects;
 
 public class Quiz {
     
+    public Guid Uuid { get; private set; }
+    
+    public string Title { get; private set; }
+
+    public int QuestionCount { get; private set; }
+
+    [JsonConstructor]
+    
+    public Quiz(
+        Guid uuid,
+        string title,
+        int questionCount
+    )
+    {
+        Uuid = uuid;
+        Title = title;
+        QuestionCount = questionCount;
+    }
 }
