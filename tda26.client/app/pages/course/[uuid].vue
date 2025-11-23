8523cf5cade9e2f57b64b1fd64064cb26bcd0be4
Jakub Sokol <sokoljakub14@gmail.com>
2025-11-23 23:40:43 +0100

    FEAT: create course/:uuid page

--- tda26.client/app/pages/course/[uuid].vue
+++ tda26.client/app/pages/course/[uuid].vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
-    import { type Course } from "#shared/types";
+import {type Course, MaterialType} from "#shared/types";
     import getBaseUrl from "#shared/utils/getBaseUrl";
+    import { NuxtLink } from '#components';
+    import Button from "~/components/Button.vue";
 
     definePageMeta({
         layout: "normal-page-layout",
@@ -24,6 +26,32 @@
                     const course = await $fetch<Course>(getBaseUrl() + `/api/v2/courses/${uuid}`);
                     const key = `course-${uuid}`;
                     const state = useState<Course | null>(key, () => null);
+                    
+                    // TODO: Odstranit po implementaci materiálů na backendu
+                    course.materials = [
+                        // Příklad materiálů
+                        { 
+                            uuid: 'mat1',
+                            name: 'Úvodní prezentace', 
+                            fileUrl: 'https://example.com/materials/intro-presentation.pdf',
+                            courseUuid: "0",
+                            description: "allah",
+                            type: MaterialType.DOCUMENT,
+                            createdAt: "2024-01-01T00:00:00Z",
+                            updatedAt: "2024-01-01T00:00:00Z"
+                        },
+                        { 
+                            uuid: 'mat2', 
+                            name: 'Studijní plán', 
+                            fileUrl: 'https://example.com/materials/study-plan.pdf',
+                            courseUuid: "0",
+                            description: "allah",
+                            type: MaterialType.DOCUMENT,
+                            createdAt: "2024-01-01T00:00:00Z",
+                            updatedAt: "2024-01-01T00:00:00Z"
+                        }
+                    ]
+                    
                     state.value = course;
                 } catch (err: any) {
 
@@ -48,6 +76,15 @@
 
     const key = `course-${uuid}`;
     const course = useState<Course>(key);
+
+    const menuItems = ['Materiály', 'Aktivita'];
+    const selectedItem = ref(menuItems[0]);
+    
+    const selectItem = (item: string) => {
+        selectedItem.value = item;
+    };
+
+    
 </script>
 
 <template>
@@ -55,11 +92,259 @@
         <Title>Kurz • Think different Academy</Title>
     </Head>
 
-    <p>{{ course.name }}</p>
-    <p>{{ course.description }}</p>
-    <p>{{ course.uuid }}</p>
+
+    <div :class="$style.course">
+        <h1 :class="['text-gradient', $style.title]">{{ course.name }}</h1>
+        <div :class="$style.info">
+            <p>{{ course.description }}</p>
+            <div :class="['liquid-glass', $style.brief]">
+                <div :class="$style.fields">
+                    <div :class="$style.el">
+                        <p :class="$style.title">Počet zhlédnutí</p>
+                        <p :class="$style.item">Samen</p>
+                    </div>
+                    <div :class="$style.el">
+                        <p :class="$style.title">Hodnocení</p>
+                        <p :class="$style.item">Samen</p>
+                    </div>
+                    <div :class="$style.el">
+                        <p :class="$style.title">Něco dalšího</p>
+                        <p :class="$style.item">Samen</p>
+                    </div>
+                </div>
+                <div :class="$style.buttons">
+                    <Button button-style="primary" accent-color="primary">Zapsat se</Button>
+                    <Button button-style="secondary" accent-color="secondary">Více informací</Button>
+                </div>
+            </div>
+        </div>
+        
+        <div :class="$style.details">
+            <nav>
+                <ul>
+                    <li
+                        v-for="item in menuItems"
+                        :key="item"
+                        :class="[item === selectedItem ? $style.active : '']"
+                        @click="selectItem(item)">
+                        {{ item }}
+                    </li>
+                </ul>
+            </nav>
+            <div :class="['liquid-glass']">
+                <div v-if="selectedItem == 'Materiály'" :class="$style.materials">
+                    <p v-if="course.materials.length == 0">Tento kurz nemá žádné materiály.</p>
+                    <ul v-else>
+                        <li v-for="material in course.materials" :key="material.uuid">
+                            <NuxtLink :href="material.fileUrl" target="_blank" rel="noopener noreferrer">
+                                <div :class="$style.fileIcon"></div>
+                                <div :class="$style.fileInfo">
+                                    <p>{{ material.name }}</p>
+                                    <div :class="$style.fileDetails">
+                                        <span>{{ material.fileUrl.match(/\.([^.]+)$/)?.[1]?.toUpperCase() ?? "Jiné" }}</span> •
+                                        <span>{{ new Date(material.createdAt).toLocaleDateString() }}</span>
+                                    </div>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+                <div v-if="selectedItem == 'Aktivita'" :class="$style.activity">
+                    <p v-if="course.feed.length == 0">Žádná nedávná aktivita.</p>
+                    <ul v-else>
+                        <li v-for="feedPost in course.feed" :key="feedPost.uuid">
+                            <!-- // TODO: -->
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </template>
 
 <style module lang="scss">
+    .title{
+        font-size: 72px;
+        margin: 0;
+        overflow: visible;
+        width: fit-content;
+    }
+    
+    ul {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+    
+    .course {
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        
+        >.info {
+            display: flex;
+            gap: 24px;
 
+            >p {
+                font-size: 18px;
+                margin-bottom: 6px;
+            }
+            
+            .brief {
+                display: flex;
+                flex-direction: column;
+                min-width: 400px;
+                gap: 24px;
+                padding: 24px;
+                border-radius: 16px;
+                box-shadow: inset 0 0 48px rgb(from var(--background-color-secondary) r g b/0.6), 0 0 8px rgba(0, 0, 0, 0.04);
+
+                >.fields {
+                    display: flex;
+
+                    >.el {
+                        position: relative;
+                        border-right: 1px solid color-mix(in srgb, var(--text-color-secondary) 30%, transparent 40%);
+                        padding: 0 24px;
+
+
+                        >.title {
+                            font-size: 16px;
+                            margin-bottom: 8px;
+                        }
+                        >.item {
+                            font-size: 24px;
+                            margin: 0;
+                        }
+
+                        &:last-child {
+                            border-right: none;
+                            padding-right: 0;
+                        }
+
+                        &:first-child {
+                            padding-left: 0;
+                        }
+                    }
+                }
+
+                >.buttons {
+                    display: flex;
+                    gap: 12px;
+                }
+            }
+        }
+        
+        
+        .details {
+            margin-top: 32px;
+            
+            nav {
+                ul {
+                    display: flex;
+                    gap: 24px;
+                    
+                    li {
+                        font-size: 18px;
+                        padding-bottom: 8px;
+                        cursor: pointer;
+                        transition: all 0.2s;
+                        user-select: none;
+                        opacity: .6;
+                        font-weight: 600;
+                        
+                        &:hover {
+                            color: var(--accent-color-secondary-theme);
+                            opacity: 1;
+                        }
+
+
+                        &::after {
+                            content: '';
+                            position: absolute;
+                            bottom: 0;
+                            left: 0;
+                            width: 100%;
+                            height: 2px;
+                            background-color: transparent;
+                            border-radius: 4px;
+                            transition: all 0.3s;
+                        }
+                        
+                        &.active {
+                            border-bottom: none;
+                            position: relative;
+                            pointer-events: none;
+                            opacity: 1;
+
+                            &::after {
+                                background-color: var(--accent-color-primary);
+                            }
+                        }
+                    }
+                }
+            }
+            
+            >div {
+                padding: 24px;
+                border-radius: 16px;
+                box-shadow: inset 0 0 48px rgb(from var(--background-color-secondary) r g b/0.6), 0 0 8px rgba(0, 0, 0, 0.04);
+                margin-top: 16px;
+            }
+
+            .materials {
+                ul {
+                    display: flex;
+                    flex-direction: column;
+                    gap: 12px;
+                    
+                    li >a {
+                        display: flex;
+                        align-items: center;
+                        gap: 6px;
+                        //box-shadow: inset 0 0 48px rgb(from var(--background-color-secondary) r g b/0.6), 0 0 8px rgba(0, 0, 0, 0.04);
+                        border: 1px solid color-mix(in srgb, var(--text-color-secondary) 10%, transparent 40%);
+                        border-radius: 12px;
+                        padding: 12px 16px;
+                        transition: background-color 0.3s;
+                        color: var(--text-color);
+                        text-decoration: none;
+                        
+                        &:hover {
+                            background-color: color-mix(in srgb, var(--accent-color-primary) 5%, var(--background-color-secondary) 95%);
+                        }
+                        
+                        .fileIcon {
+                            mask-image: url('../../../public/icons/file.svg');
+                            mask-size: cover;
+                            mask-position: center;
+                            mask-repeat: no-repeat;
+
+                            width: 24px;
+                            height: 24px;
+                            background-color: var(--text-color-secondary);
+                            opacity: 0.6;
+                        }
+                        
+                        .fileInfo {
+                            display: flex;
+                            flex-direction: column;
+                            gap: 4px;
+
+                            p {
+                                margin: 0;
+                                font-size: 16px;
+                            }
+
+                            .fileDetails {
+                                font-size: 12px;
+                                color: var(--text-color-secondary);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
--- tda26.client/shared/types.ts
+++ tda26.client/shared/types.ts
@@ -6,6 +6,9 @@ export interface Course {
     description: string;
     createdAt: string;
     updatedAt: string;
+    materials: Materials[];
+    quizzes: Quiz[];
+    feed: FeedPost[];
 }
 
 export interface Lecturer {
@@ -27,8 +30,42 @@ export interface Lecturer {
     updatedAt: string,
 }
 
-export interface Account extends Lecturer {
-    username: string
+export interface Account extends Lecturer{
+    username: string,
+}
+
+export interface Materials {
+    uuid: string,
+    type: MaterialType,
+    courseUuid: string,
+    name: string,
+    description: string,
+    fileUrl: string,
+    createdAt: string,
+    updatedAt: string,
+}
+
+export enum MaterialType {
+    VIDEO = "video",
+    DOCUMENT = "document",
+    AUDIO = "audio",
+    IMAGE = "image"
+}
+
+export interface Quiz {
+    uuid: string,
+    courseUuid: string,
+    title: string,
+    questionCount: string,
+    createdAt: string,
+    updatedAt: string,
+}
+
+export interface FeedPost {
+    uuid: string,
+    createdAt: string,
+    updatedAt: string,
+    courseUuid: string,
 }
 
 export type ClassLike = string | undefined | Record<string, boolean | null | undefined> | ClassLike[];
