1bec7d1d5323dcee7fa743af82335d4c2dc11d16
Jakub Sokol <sokoljakub14@gmail.com>
2025-12-25 23:36:15 +0100

    FIX: error if ImageUrl is null

--- tda26.Server/DTOs/Mapping/CourseMapper.cs
+++ tda26.Server/DTOs/Mapping/CourseMapper.cs
@@ -20,7 +20,9 @@ public static class CourseMapper
             Description = course.Description,
             CreatedAt = course.CreatedAt,
             UpdatedAt = course.UpdatedAt,
-            ImageUrl = course.ImageUrl.StartsWith("course-images/") ? "api/v2/courses/" + course.Uuid + "/image" : null,
+            ImageUrl = course.ImageUrl != null && course.ImageUrl.StartsWith("course-images/")
+                ? $"api/v2/courses/{course.Uuid}/image"
+                : null,
             Lecturer = course.Lecturer,
             Account = course.Account,
             ViewCount = course.ViewCount,
