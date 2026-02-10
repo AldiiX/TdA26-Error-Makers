4c701469b546c36439530310c2bb9a5eab9b3e9f
Serhii Yavorskiy <yavorskiyserhiy158@gmail.com>
2026-02-10 21:42:03 +0100

    Use external done.svg for multipleChoiceIcon
    Replace the inline data-URI mask-image in QuizQuestionCard.vue with a reference to /icons/done.svg and add the new SVG file at public/icons/done.svg. This moves the checkmark icon out of the CSS data URI into a standalone asset (easier to edit and cache).

--- tda26.client/app/components/pagespecific/QuizQuestionCard.vue
+++ tda26.client/app/components/pagespecific/QuizQuestionCard.vue
@@ -372,7 +372,7 @@ const isDeleteModalOpen = ref(false);
         }
         
         .multipleChoiceIcon {
-            mask-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2'%3E%3Crect x='3' y='3' width='18' height='18' rx='2'/%3E%3Cpath d='M9 12l2 2 4-4' stroke-width='2.5'/%3E%3C/svg%3E");
+            mask-image: url("/icons/done.svg");
         }
     }
 
