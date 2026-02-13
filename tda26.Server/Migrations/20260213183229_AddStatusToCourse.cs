684da4a572eaa9c30ffd396d4045ff2ab3f2bd11
Jakub Sokol <sokoljakub14@gmail.com>
2026-02-13 21:08:27 +0100

    FEAT: add status and scheduled start properties to Course model

---
+++ tda26.Server/Migrations/20260213183229_AddStatusToCourse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tda26.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToCourse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ScheduledStart",
+                table: "Courses",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScheduledStart",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Courses");
+        }
+    }
+}
